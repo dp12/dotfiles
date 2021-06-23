8e6c19316390ace3a87998274857a794bae163f7
dp12 <deep.paren.12@gmail.com>
2021-06-23 16:33:58 -0400

    Add rg byte search scripts

---
+++ python-scripts/rgbl.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+if len(sys.argv) < 2:
+    print("usage: rgb <hexstring>")
+
+hexstring = sys.argv[1]
+extra_args = ' '.join(sys.argv[2:])
+rg_cmd = "rg -boal"
+search_string = "\"(?-u)"
+for i in range(0, len(hexstring), 2):
+    search_string += "\\x" + hexstring[i:i+2]
+search_string += "\""
+cmd = rg_cmd + " " + search_string + " " + extra_args
+os.system(cmd)
