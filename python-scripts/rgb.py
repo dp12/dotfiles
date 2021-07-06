3640a9165a40e22ebc321760f1cc2d75f0623755
dp12 <deep.paren.12@gmail.com>
2021-07-06 15:25:50 -0400

    Update rgb script

--- python-scripts/rgb.py
+++ python-scripts/rgb.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 import sys
 
-if len(sys.argv) < 2:
-    print("usage: rgb <hexstring>")
-    sys.exit()
+parser = argparse.ArgumentParser()
+parser.add_argument('hexstring')
+parser.add_argument("-l", "--files_with_matches", help="only print paths",
+                    action="store_true")
+parser.add_argument("-r", "--reverse", help="reverse the hex string provided",
+                    action="store_true")
+parser.add_argument('rest', nargs=argparse.REMAINDER)
+args = parser.parse_args()
 
-hexstring = sys.argv[1]
-extra_args = ' '.join(sys.argv[2:])
-rg_cmd = "rg -boa"
+hexstring = args.hexstring
+if hexstring.startswith("0x"):
+    hexstring = hexstring[2:]
+
+if args.reverse:
+    hexstring = "".join(reversed([hexstring[i:i+2] for i in range(0, len(hexstring), 2)]))
+
+if args.files_with_matches:
+    rg_cmd = "rg -boal"
+else:
+    rg_cmd = "rg -boa"
+extra_args = ' '.join(args.rest)
+
 search_string = "\"(?-u)"
 for i in range(0, len(hexstring), 2):
     search_string += "\\x" + hexstring[i:i+2]
